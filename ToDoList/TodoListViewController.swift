47284f9a4ae441e13587841fbc5afaf332cff562
GeorgeThoppil <georgethoppil2@gmail.com>
2017-12-27 23:43:48 -0500

    Added alert dialog to add to do list items

--- ToDoList/TodoListViewController.swift
+++ ToDoList/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
 
-    let itemArray = ["Find mike", "Buy Eggos", "Destory Demogorgaon"]
+    var itemArray = ["Find mike", "Buy Eggos", "Destory Demogorgaon"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -46,5 +46,22 @@ class TodoListViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textField = UITextField()
+        
+        let alert = UIAlertController(title: "Add new todo item", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new item"
+            textField = alertTextField
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
